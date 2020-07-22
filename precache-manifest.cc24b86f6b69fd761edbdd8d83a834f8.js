6d99392df8fdc7863143c3eeecb857cf6b3d76c3
Salah Tomezy <salahidna@gmail.com>
2020-07-22 20:26:59 +0300

    Updates

---
+++ precache-manifest.cc24b86f6b69fd761edbdd8d83a834f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f94b75be5cf8bd160fa44f66392e3ab",
+    "url": "/TODO-List/index.html"
+  },
+  {
+    "revision": "2ebfd873aa21034db536",
+    "url": "/TODO-List/static/css/main.49a0d269.chunk.css"
+  },
+  {
+    "revision": "9b1609b22c35c6b43f80",
+    "url": "/TODO-List/static/js/2.df75b0be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TODO-List/static/js/2.df75b0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebfd873aa21034db536",
+    "url": "/TODO-List/static/js/main.8e730f8c.chunk.js"
+  },
+  {
+    "revision": "00b306b6bd971528c916",
+    "url": "/TODO-List/static/js/runtime-main.2daa5087.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TODO-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
